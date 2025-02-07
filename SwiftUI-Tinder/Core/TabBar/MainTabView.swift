172fd072d07a36fe92166d4f2ecd99473943c34e
Chirag Kular <ck4957@rit.edu>
2025-02-07 11:20:53 -0500

    Add Folder Structure

---
+++ SwiftUI-Tinder/Core/TabBar/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  SwiftUI-Tinder
+//
+//  Created by Chirag Kular on 2/7/25.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}

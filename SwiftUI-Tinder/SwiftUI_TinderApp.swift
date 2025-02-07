e79100ec05144cf0697a357d4ee89566b7b38be4
Chirag Kular <ck4957@rit.edu>
2025-02-07 11:14:22 -0500

    Initial Commit

---
+++ SwiftUI-Tinder/SwiftUI_TinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TinderApp.swift
+//  SwiftUI-Tinder
+//
+//  Created by Chirag Kular on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
